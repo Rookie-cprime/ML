d824190dcab8bfb40c64ad19983c4a0a0fe4dd0a
YanxinSun <syxsdhr@163.com>
2020-09-26 10:22:13 +0800

    1

--- LGD/main.py
+++ LGD/main.py
@@ -1,38 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
-class AdalineGD(object):
-    """ADAptive LInear NEuron classifier.
-    Parameters
-    ----------
-    eta: float
-    Learning rate (between 0.0 and 1.0)
-    n_iter : int
-    Passes over the training dataset
-    random_state:int
-    Random number generator seed for random weight
-    initialization.
+from AdalineGD import *
 
-    Attributes
-    ------------
-    w_: Id-array
-    Weights after fitting
-    cost_: list
-    Sum-of-squares cost function value in each epoch
-    """
-    def __init__(self,eta = 0.01, n_iter = 50,random_state=1):
-        self.eta = eta
-        self.n_iter = n_iter
-        self.random_state = random_state
-    def fit(self,X,y):
-        """
+df = pd.read_csv('E:/GIT/ML/LGD/iris.data',header = None)
+df.tail()
+#select setosa and versicolor
+y = df.iloc[0:100,4].values
+y = np.where(y == "Iris-setosa",-1,1)
+#extract sepal length and petal length
+X = df.iloc[0:100,[0,2]].values
 
-        :param X: {array_like},shape = {n_samples,n_features}
-        Training vectors,where n_samples is the number of samples
-        and n_features is the number of features.
-        :param y: array-like,shape = [n_sample]
-        Target value
-        :return:
-        self:object
-        """
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc = 0.0,scale = 0.01,size = 1+X.shape[1])#loc everage scale sdandard size
-        
+fig,ax=plt.subplots(nrows = 1,ncols = 2,figsize = (10,4))
+ada1 = AdalineGD(n_iter = 10,eta = 0.01)
+ada1.fit(X,y)
+ax[0].plot(range(1,len(ada1.cost_)+1),np.log10(ada1.cost_),marker = 'o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter = 10,eta = 0.0001)
+ada2.fit(X,y)
+ax[1].plot(range(1,len(ada2.cost_)+1),np.log10(ada2.cost_),marker = 'o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('log(Sum-squared-error)')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.show()
